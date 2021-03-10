8cde36a321c95f787cb79ca2990535955a8b05da
mohamedfawzy96 <mohamedfawzydes@gmail.com>
2021-03-10 17:36:01 +0100

    refactoring

--- tap_hubspot/streams/deal_by_companies.py
+++ tap_hubspot/streams/deal_by_companies.py
@@ -4,9 +4,6 @@ from .deal import Deal
 
 class DealByCompany(Associations):
     tap_stream_id = "deals_by_company"
-    key_properties = ["id"]
-    replication_key = "created_at"
-    replication_method = "INCREMENTAL"
 
     first_resource_name = "DEALS"
     second_resource_name = "COMPANIES"
